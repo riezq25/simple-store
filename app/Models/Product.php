c0b164bfb234b56485dd793d75fbbf70efb264cd
Rizqi Ari Putra <riezq.25@gmail.com>
2024-10-28 09:43:43 +0700

    [FEAT] product table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Product extends Model
 {
     use HasFactory;
 
-    public $table = 'categories';
-    public $primaryKey = 'category_id';
+    public $table = 'products';
+    public $primaryKey = 'product_id';
 
     protected $guarded = [
         'category_name'
