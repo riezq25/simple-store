accf2845eb4a97c23a68dec645dcbbf802bfe931
Rizqi Ari Putra <riezq.25@gmail.com>
2024-10-29 21:51:42 +0700

    [FIX] Dashboard view

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -27,7 +27,8 @@
                                 </div>
                                 <div class="flex-grow-1">
                                     <h6 class="mb-0">{{ auth()->user()->name }}</h6>
-                                    <small class="text-muted">Admin</small>
+                                    <small
+                                        class="text-muted">{{ strtoupper(auth()->user()?->roles()?->first()?->name) ?? '-' }}</small>
                                 </div>
                             </div>
                         </a>
