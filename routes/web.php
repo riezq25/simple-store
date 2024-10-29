f4ef531d906f7a3dddaf1645e2fde7041585b763
Rizqi Ari Putra <riezq.25@gmail.com>
2024-10-29 21:54:32 +0700

    [FEAT] Route

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,24 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index'])
-    ->middleware(['auth'])
-    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Dashboard
+    Route::get('/', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+    // Transaksi
+
+    // Data Master
+    // a. Kategori
+    // b. Produk
+    // c. Supplier
+
+    // Pengguna
+    // a. customer
+    // b. admin
+
+    // profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
