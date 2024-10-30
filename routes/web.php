ae0541f0dad956ed66df0659ba475b7437cca185
Rizqi Ari Putra <riezq.25@gmail.com>
2024-10-30 08:13:29 +0700

    [FEAT] Remove for training

--- routes/web.php
+++ routes/web.php
@@ -13,47 +13,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])
         ->name('dashboard');
 
-    // Transaksi
-
+    // Riwayat Transaksi
 
     // admin only
     Route::middleware('role:admin')->group(function () {
         // Data Master
         // a. Kategori
         // b. Produk
-
         // c. Supplier
-        Route::get('/data-master/supplier', [SupplierController::class, 'index'])
-            ->name('supplier.index');
-        Route::get('/data-master/supplier/create', [SupplierController::class, 'create'])
-            ->name('supplier.create');
-        Route::post('/data-master/supplier/store', [SupplierController::class, 'store'])
-            ->name('supplier.store');
-        Route::get('/data-master/supplier/{id}/edit', [SupplierController::class, 'edit'])
-            ->name('supplier.edit');
-        Route::put('/data-master/supplier/{id}/update', [SupplierController::class, 'update'])
-            ->name('supplier.update');
-        Route::delete('/data-master/supplier/{id}', [SupplierController::class, 'destroy'])
-            ->name('supplier.destroy');
+
 
         // Pengguna
         // a. customer
-        Route::get('/pengguna/customer', [CustomerController::class, 'index'])
-            ->name('customer.index');
-
         // b. admin
-        Route::get('/pengguna/admin', [AdminController::class, 'index'])
-            ->name('admin.index');
-        Route::get('/pengguna/admin/create', [AdminController::class, 'create'])
-            ->name('admin.create');
-        Route::post('/pengguna/admin/store', [AdminController::class, 'store'])
-            ->name('admin.store');
-        Route::get('/pengguna/admin/{id}/edit', [AdminController::class, 'edit'])
-            ->name('admin.edit');
-        Route::put('/pengguna/admin/{id}/update', [AdminController::class, 'update'])
-            ->name('admin.update');
-        Route::delete('/pengguna/admin/{id}', [AdminController::class, 'destroy'])
-            ->name('admin.destroy');
     });
 
 
